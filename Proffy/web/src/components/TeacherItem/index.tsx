506f9d314c2aba7fba22f02095cae7b60ad6e602
Khalil Novaes <khalil.uxdev@gmail.com>
2020-08-08 05:56:03 -0300

    Dia 3

--- Proffy/web/src/components/TeacherItem/index.tsx
+++ Proffy/web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+     
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://pbs.twimg.com/profile_images/1284184681269911552/b69ywdav_400x400.jpg" alt="Khalil Novaes"/>
+                <img src={teacher.avatar} alt= {teacher.name} />
                 <div>
-                    <strong>Khalil Novaes</strong>
-                    <span>Matematica</span>
+                    <strong>{teacher.name}</strong>
+                 <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-            Ah mano sei la sabe.
-            <br /><br />
-            Por conseguinte, a constante divulgação das informações imponha um obstáculo ao upgrade para novas versões das direções preferenciais na escolha de algorítimos. O empenho em analisar a valorização de fatores subjetivos possibilita uma melhor disponibilidade da autenticidade das informações.
-            </p>
+            <p> {teacher.bio} </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 70,00</strong>
+                    <strong>R$ {teacher.cost} </strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
          </article>
     );
